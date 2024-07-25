1fb37b5f7e9a77da3ea80c95e6fa3c49048da547
Matt Knop <mknop@redhat.com>
2024-07-25 14:55:00 -0600

    adding flask cors support

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS, cross_origin
 
 from connectors.vector_store.db import db, db_connection_string, vector_interface
 from resources.routes import initialize_routes
 
 app = Flask(__name__)
+cors = CORS(app)
 
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = db_connection_string
 
 db.init_app(app)
