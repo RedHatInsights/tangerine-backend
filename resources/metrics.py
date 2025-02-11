ab55246fdc2fe88bbff06c41c150e8e8838de430
Brandon Squizzato <35474886+bsquizz@users.noreply.github.com>
2025-02-11 12:42:42 -0500

    Add prefix for metrics (#108)

--- resources/metrics.py
+++ resources/metrics.py
@@ -1,12 +1,16 @@
 from prometheus_client import REGISTRY, Counter, Gauge
 from prometheus_flask_exporter import RESTfulPrometheusMetrics
 
-metrics = RESTfulPrometheusMetrics.for_app_factory(registry=REGISTRY)
+import connectors.config as cfg
+
+metrics = RESTfulPrometheusMetrics.for_app_factory(
+    registry=REGISTRY, defaults_prefix=cfg.METRICS_PREFIX
+)
 
 
 def get_counter(name: str, description: str) -> Counter:
-    return Counter(name, description)
+    return Counter(f"{cfg.METRICS_PREFIX}_{name}", description)
 
 
 def get_gauge(name: str, description: str) -> Gauge:
-    return Gauge(name, description)
+    return Gauge(f"{cfg.METRICS_PREFIX}_{name}", description)
