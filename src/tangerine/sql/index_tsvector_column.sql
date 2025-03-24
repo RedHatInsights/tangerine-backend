0f166d13d18752b428601bbb868c70cbc374111a
Adam Drew <addrew@redhat.com>
2025-03-24 11:03:23 -0400

    Add new SQL queries for pgsearch FTS support

---
+++ src/tangerine/sql/index_tsvector_column.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_langchain_pg_embedding_fts
+ON langchain_pg_embedding USING GIN (fts_vector)
---
+++ src/tangerine/sql/tsvector_check.sql
@@ -0,0 +1,2 @@
+SELECT column_name FROM information_schema.columns
+WHERE table_name = 'langchain_pg_embedding' AND column_name = 'fts_vector'
