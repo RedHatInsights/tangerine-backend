fa2ee00cf08780d2bea4a834713525e16cf08fd6
Brandon Squizzato <35474886+bsquizz@users.noreply.github.com>
2025-03-26 12:32:47 -0400

    Tweak search SQL, add filter for active=True, remove score cutoffs (#152)

--- src/tangerine/sql/hybrid_search.sql
+++ src/tangerine/sql/hybrid_search.sql
@@ -1,14 +1,38 @@
+WITH fts_results AS (
+    SELECT
+        id,
+        document,
+        cmetadata,
+        ts_rank_cd(fts_vector, plainto_tsquery('english', :query)) AS fts_rank
+    FROM
+        langchain_pg_embedding
+    WHERE
+        cmetadata->>'agent_id' = :agent_id AND cmetadata->>'active' = 'True' AND fts_vector @@ plainto_tsquery('english', :query)
+),
+vector_results AS (
+    SELECT
+        id,
+        document,
+        cmetadata,
+        -(embedding <#> :embedding) AS vector_rank
+    FROM
+        langchain_pg_embedding
+    WHERE
+        cmetadata->>'agent_id' = :agent_id AND cmetadata->>'active' = 'True'
+    ORDER BY
+        vector_rank DESC
+    LIMIT 100  -- Limit the number of candidates to refine the query performance
+)
 SELECT
-  id,
-  document,
-  cmetadata,
-  ts_rank_cd(fts_vector, plainto_tsquery(:query)) AS fts_score,
-  1 - (embedding <#> :embedding) AS vector_score,
-  (
-    0.7 * ts_rank_cd(fts_vector, plainto_tsquery(:query)) + -- weighting for FTS score
-    0.3 * (1 - (embedding <#> :embedding))              -- weighting for vector score
-  ) AS hybrid_score
-FROM langchain_pg_embedding
-WHERE cmetadata->>'agent_id' = :agent_id
-ORDER BY hybrid_score DESC
+    fts_results.id,
+    fts_results.document,
+    fts_results.cmetadata,
+    (1 / (1 + fts_results.fts_rank)) + (1 / (1 + vector_results.vector_rank)) AS rrf_score
+FROM
+    fts_results
+JOIN
+    vector_results
+    ON fts_results.id = vector_results.id
+ORDER BY
+    rrf_score DESC
 LIMIT 4;
