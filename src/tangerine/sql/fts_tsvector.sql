75da6dccffefb6b9c9844f94d1721d5a446f85a9
Brandon Squizzato <bsquizza@redhat.com>
2025-03-26 22:24:39 -0400

    Fix param name

--- src/tangerine/sql/fts_tsvector.sql
+++ src/tangerine/sql/fts_tsvector.sql
@@ -2,13 +2,13 @@ SELECT
     id,
     document,
     cmetadata,
-    ts_rank_cd(fts_vector, plainto_tsquery('english', :q)) AS score
+    ts_rank_cd(fts_vector, plainto_tsquery('english', :query)) AS score
 FROM
     langchain_pg_embedding
 WHERE
     cmetadata->>'agent_id' = :agent_id
     AND cmetadata->>'active' = 'True'
-    AND fts_vector @@ plainto_tsquery('english', :q)
+    AND fts_vector @@ plainto_tsquery('english', :query)
 ORDER BY
     score DESC
 LIMIT 4;
