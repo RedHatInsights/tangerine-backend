9f67a243998c1e7337aee73fae2451d4be55e2a7
Brandon Squizzato <bsquizza@redhat.com>
2025-03-26 21:32:05 -0400

    Format fts query for easier readability

--- src/tangerine/sql/fts_tsvector.sql
+++ src/tangerine/sql/fts_tsvector.sql
@@ -1,5 +1,14 @@
-SELECT id, document, cmetadata, ts_rank_cd(fts_vector, plainto_tsquery('english', :q)) AS score
-FROM langchain_pg_embedding
-WHERE cmetadata->>'agent_id' = :agent_id AND cmetadata->>'active' = 'True' AND fts_vector @@ plainto_tsquery('english', :q)
-ORDER BY score DESC
+SELECT
+    id,
+    document,
+    cmetadata,
+    ts_rank_cd(fts_vector, plainto_tsquery('english', :q)) AS score
+FROM
+    langchain_pg_embedding
+WHERE
+    cmetadata->>'agent_id' = :agent_id
+    AND cmetadata->>'active' = 'True'
+    AND fts_vector @@ plainto_tsquery('english', :q)
+ORDER BY
+    score DESC
 LIMIT 4;
